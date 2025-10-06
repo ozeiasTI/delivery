4c11d2808cbf86f89080d9a7d663f638203a721b
Ozeias Meira Santos de Souza <3306388@ifro.local>
2025-10-06 12:35:32 -0400

    aula-01

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { styles } from './style';
+import Button from './src/components/button/button';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Heelo Word</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        <Text style={styles.textos}>Hello Word</Text>
+        <Button texto='Botao'/>
+      </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/constants/theme.js
@@ -0,0 +1,20 @@
+const COLORS = {
+    red: '#ED5359',
+    darkgray : '#5D6275',
+    mediumGray: '#A6ABBC',
+    lightGray: '#E5ECFC',
+    white : '#F4F7FE',
+    green : '#38C682'
+}
+
+const FONT_SIZE = {
+    xsm : 11,
+    sm : 14,
+    md : 16,
+    lg: 22,
+    xl: 32,
+    xxl: 64
+} 
+
+
+export {COLORS, FONT_SIZE};
